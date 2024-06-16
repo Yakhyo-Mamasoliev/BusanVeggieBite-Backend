f35f28126b5d4ef0d2e55a868da5c4e9fcbbf67f
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-06-16 14:34:00 +0900

    fix: modify restaurant login signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,20 +2,23 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
   private readonly memberModel;
-
   constructor() {
-    this.memberModel = MemberModel; // memberModel is class, exception for lowercase names
-  } // Constructor method
-
+    this.memberModel = MemberModel;
+  }
   public async processSignup(input: MemberInput): Promise<Member> {
     const exist = await this.memberModel
       .findOne({ memberType: MemberType.RESTAURANT })
       .exec(); // exec() is chain query method,
     console.log("exist: ", exist);
     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+
     try {
       const result = await this.memberModel.create(input);
       result.memberPassword = ""; //       // just for testing if memberSchemaModel is class// const tempResult = new this.memberModel(input);// const result = await tempResult.save();// if you need string you can write// Promise is used when method is asynchronous
@@ -34,7 +37,11 @@ class MemberService {
       .exec();
     if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
 
-    const isMatch = input.memberPassword === member.memberPassword;
+    const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+    );
+
     if (!isMatch)
       throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
 
