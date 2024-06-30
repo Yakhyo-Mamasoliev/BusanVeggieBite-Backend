773753f78bc38bcaa1b29f9aeccbc169fed0276f
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-06-30 18:44:29 +0900

    feat: signup form validation and image upload functionality

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,29 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let fileName;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const validationType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validationType.includes(fileType)) {
+        alert("Only image/jpg, image/jpeg, image/png");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        fileName = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(fileName);
+    }
+  });
+});
 
 function validateSignupForm() {
   const memberNick = $(".member-nick").val();
@@ -19,7 +42,15 @@ function validateSignupForm() {
   }
 
   if (memberPassword !== confirmPassword) {
-    alert("The password differs, please check!");
+    alert("The password differs,  please check!");
+    return false;
+  }
+
+  const memberImage = $(".member-image").get(0).files[0].name
+    ? $(".member-image").get(0).files[0].name
+    : null;
+  if (!memberImage) {
+    alert("Please insert restaurant image");
     return false;
   }
 }
