5990af0cf7bcc83e1af22170e01162d19a41a82e
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-07-05 10:24:33 +0900

    feat: MITASK-P completion

--- src/train.ts
+++ src/train.ts
@@ -1,22 +1,34 @@
-// P-TASK:
+// Q-TASK:
 
-// Shunday function yozing, u object qabul qilsin va arrayni object arrayga otkazib arrayni qaytarsin qaytarsin.
-// MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
+// Shunday function yozing, u 2 ta parametrgga ega bolib birinchisi object, ikkinchisi string. Agar string parametr objectni propertysi bolsa true bolmasa false qaytarsin.
+// MASALAN: hasProperty({name: "BMW", model: "M3"}, "model") return true; hasProperty({name: "BMW", model: "M3"}, "year") return false
 
-function objectToArray(obj: { [key: string]: any }): [string, any][] {
-  const result: [string, any][] = [];
-
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      result.push([key, obj[key]]);
-    }
-  }
-  return result;
+function hasProperty(obj: Record<string, any>, property: string): boolean {
+  return obj.hasOwnProperty(property);
 }
 
-const exampleObject = { a: 10, b: 20 };
-const result = objectToArray(exampleObject);
-console.log("Masalan:", result);
+console.log(hasProperty({ name: "BMW", model: "M3" }, "model")); // true
+console.log(hasProperty({ name: "BMW", model: "M3" }, "year")); // false
+
+// // P-TASK:
+
+// // Shunday function yozing, u object qabul qilsin va arrayni object arrayga otkazib arrayni qaytarsin qaytarsin.
+// // MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
+
+// function objectToArray(obj: { [key: string]: any }): [string, any][] {
+//   const result: [string, any][] = [];
+
+//   for (const key in obj) {
+//     if (obj.hasOwnProperty(key)) {
+//       result.push([key, obj[key]]);
+//     }
+//   }
+//   return result;
+// }
+
+// const exampleObject = { a: 10, b: 20 };
+// const result = objectToArray(exampleObject);
+// console.log("Masalan:", result);
 
 // // O-TASK:
 // // Shunday function yozing, u har xil valuelardan iborat array qabul qilsin va array ichidagi sonlar yigindisini hisoblab chiqqan javobni qaytarsin.
