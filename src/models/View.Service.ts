a6b61d3e6a9695417e7af38009eea662aded67c3
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-07-24 17:50:10 +0900

    feat: create view schema and service models

---
+++ src/models/View.Service.ts
@@ -0,0 +1,11 @@
+import ViewModel from "../schema/View.Model";
+
+class ViewService {
+  private readonly viewModel;
+
+  constructor() {
+    this.viewModel = ViewModel;
+  }
+}
+
+export default ViewService;
