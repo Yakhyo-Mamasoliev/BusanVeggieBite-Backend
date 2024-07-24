b603ea50353bd50528a29285c4f2506222261422
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-07-24 18:57:51 +0900

    fix: modify getProduct business logic

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,3 +1,4 @@
+import { ViewInput } from "./../libs/types/view";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 import { ProductStatus } from "../libs/enums/product.enum";
 import Errors, { HttpCode, Message } from "../libs/Errors";
@@ -10,12 +11,16 @@ import {
 } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 import { ObjectId } from "mongoose";
+import ViewService from "./View.Service";
+import { ViewGroup } from "../libs/enums/view.enum";
 
 class ProductService {
   private readonly productModel;
+  public viewService;
 
   constructor() {
     this.productModel = ProductModel;
+    this.viewService = new ViewService();
   }
 
   /** SPA */
@@ -61,7 +66,31 @@ class ProductService {
       .exec();
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
-    // TODO: If authenticated users => first => view log creation
+    if (memberId) {
+      // Check Existence
+      const input: ViewInput = {
+        memberId: memberId,
+        viewRefId: productId,
+        viewGroup: ViewGroup.PRODUCT,
+      };
+      const existView = await this.viewService.checkViewExistence(input);
+
+      // Insert new View log
+      console.log("exist:", !!existView); // if exist works, below does not work => one user one view
+      if (!existView) {
+        // insert View
+        await this.viewService.insertMemberView(input);
+      }
+
+      // Increase Counts
+      result = await this.productModel
+        .findByIdAndUpdate(
+          productId,
+          { $inc: { productViews: +1 } },
+          { new: true }
+        )
+        .exec();
+    }
 
     return result;
   }
