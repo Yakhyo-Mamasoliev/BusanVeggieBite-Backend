954c0f32d7184c0df6cd4b2c62d2ebb7d73974e2
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-07-26 19:07:21 +0900

    feat: develop getMyOrders rest api

--- src/router.ts
+++ src/router.ts
@@ -41,4 +41,10 @@ router.post(
   memberController.verifyAuth,
   orderController.createOrder
 );
+
+router.get(
+  "/order/all",
+  memberController.verifyAuth,
+  orderController.getMyOrders
+);
 export default router;
